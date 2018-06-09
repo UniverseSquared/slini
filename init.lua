d781ed2fd1717bd7f1c0f2aca105c3c12b851b6e
UniverseSquared <universesquared4@gmail.com>
2018-06-09 14:25:26 +0100

    Basic ini parsing

---
+++ init.lua
@@ -0,0 +1,25 @@
+local slini = {}
+
+function slini.parse(data)
+	-- Add trailing \n
+	if data:sub(-1) ~= "\n" then
+		data = data .. "\n"
+	end
+	
+	local out = {}
+	local section = nil
+	for line in data:gmatch("(.-)\n") do
+		local s = line:match("^%[(.-)%]$")
+		if s ~= nil then
+			section = s
+			out[section] = {}
+		else
+			local key, value = line:match("^(.-)=(.-)$")
+			out[section][key] = value
+		end
+	end
+
+	return out
+end
+
+return slini
